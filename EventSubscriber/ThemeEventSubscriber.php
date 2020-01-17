22cb49146351e5513645833261b72c37fda8ddd5
lduer <duerlukas@gmail.com>
2020-01-17 18:12:26 +0100

    added default js/css to theme (ThemeEvent)

--- EventSubscriber/ThemeEventSubscriber.php
+++ EventSubscriber/ThemeEventSubscriber.php
@@ -12,11 +12,18 @@ namespace KimaiPlugin\CustomTimesheetFormBundle\EventSubscriber;
 use App\Event\ThemeEvent;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 final class ThemeEventSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var \Twig\Environment
+     */
     private $twig;
 
+    /**
+     * @var Packages
+     */
     private $packages;
 
     public function __construct(\Twig\Environment $twig, Packages $packages)
@@ -41,9 +48,21 @@ final class ThemeEventSubscriber implements EventSubscriberInterface
      */
     public function renderContentAfter(ThemeEvent $event)
     {
-        $url = $this->packages->getUrl('bundles/customtimesheetform/js/visible-media-query.js');
+        $html = '';
 
-        $html = '
+        // timesheets can be edited/created from all pages via toolbar.
+        // add timepicker scripts/css to all html pages
+
+        $html .='
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.css"
+          integrity="sha256-zV9aQFg2u+n7xs0FTQEhY0zGHSFlwgIu7pivQiwJ38E=" crossorigin="anonymous"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.js"
+            integrity="sha256-xoE/2szqaiaaZh7goVyF5p9C/qBu9dM3V5utrQaiJMc=" crossorigin="anonymous"></script>
+';
+
+
+        $url = $this->packages->getUrl('bundles/customtimesheetform/js/visible-media-query.js');
+        $html .= '
     <script src="' . $url . '"></script>
     <div class="device-xs visible-xs-block"></div>
     <div class="device-sm visible-sm-block"></div>
